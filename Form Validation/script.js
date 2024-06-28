72a03eee2bbb5e7c76d872931283340b66ac4568
Krushna sahoo <krushnasahoo706@gmail.com>
2024-06-28 21:49:48 +0530

    idk

--- Form Validation/script.js
+++ Form Validation/script.js
@@ -1,12 +1,38 @@
-const email=document.getElementById('email');
-const country=document.getElementById('email');
-const pincode=document
-.getElementById('picode');
-const password=document.getElementById('password');
+const email = document.getElementById('email');
+const country = document.getElementById('country');
+const pincode = document.getElementById('pincode');
+const password = document.getElementById('password');
 
+function pressed() {
+    let isValid = true;
 
-function pressed(){
-    if(!email.checkValidity()){
-        
+    if (!email.checkValidity()) {
+        document.getElementById('e_valid').innerHTML = email.validationMessage;
+        isValid = false;
+    } else {
+        document.getElementById('e_valid').innerHTML = '';
     }
+
+    if (!country.checkValidity()) {
+        document.getElementById('c_valid').innerHTML = country.validationMessage;
+        isValid = false;
+    } else {
+        document.getElementById('c_valid').innerHTML = '';
+    }
+
+    if (!pincode.checkValidity()) {
+        document.getElementById('pin_valid').innerHTML = pincode.validationMessage;
+        isValid = false;
+    } else {
+        document.getElementById('pin_valid').innerHTML = '';
+    }
+
+    if (!password.checkValidity()) {
+        document.getElementById('pas_valid').innerHTML = password.validationMessage;
+        isValid = false;
+    } else {
+        document.getElementById('pas_valid').innerHTML = '';
+    }
+
+    return isValid; // Return false if form is not valid to prevent submission
 }
