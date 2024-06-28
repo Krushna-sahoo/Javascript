1e92ff87ca399ec5a07b52ff7cc71013a1c9557c
Krushna sahoo <krushnasahoo706@gmail.com>
2024-06-28 21:31:15 +0530

    validation form

---
+++ Form Validation/script.js
@@ -0,0 +1,12 @@
+const email=document.getElementById('email');
+const country=document.getElementById('email');
+const pincode=document
+.getElementById('picode');
+const password=document.getElementById('password');
+
+
+function pressed(){
+    if(!email.checkValidity()){
+        
+    }
+}
